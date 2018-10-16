c903f1898848a264247071de7cdb9027b12cfb77
wangff15386 <314130948@qq.com>
2018-10-16 23:21:01 +0800

    add interface

--- test/getbalance_test.js
+++ test/getbalance_test.js
@@ -2,10 +2,12 @@ const dice = require("../dice/dice")
 
 dice.GetBalance("0xd3ee9ab572ed74f0b837ad9ea86f85e30e1dd6d1", function(err, data){
     console.log("callback")
-    console.log(data)
     if (err){
+        console.log(err)
         console.log("callback Error")
         return
     }
+    
+    console.log(data)
     console.log("callback Success")
 })
