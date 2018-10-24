9503355a6874b27b29ca43e4930e2b03ce035940
MiyaFlower <yangmingyan1987.com>
2018-10-24 17:29:32 +0800

    2018-10-24 aes encryption decrypt sessionStorage set get Account balance login（Finish 40%） roll（Finish 30%）

--- game/js/seele-dice.js
+++ game/js/seele-dice.js
@@ -2,7 +2,46 @@
 author: Miya_yang
 time:2018.10.11
 */
-$(document).ready(function($) {
+const dice = require('dice.js')
+// const seelejs = require('seele.js')
+// callback function
+// function tryMe (param) { 
+//   console.log(param)
+// } 
+// // callback executer 
+// function callbackTester (callback) { 
+//   dice.GetBalance(dice.ContractAddress, function (data) {
+//     callback(data.Balance)
+//   })
+// } 
+
+// // test function
+// callbackTester (function(k) {
+//   tryMe(k)
+// })
+// function tryMe(param) {
+//   console.log(param1);
+// }
+// dice.GetBalance(dice.ContractAddress, function (data) {
+//   callback(data.Balance)
+// })
+
+// // function callbackTester(callback) {
+// //   callback(arguments[1], arguments[2]);
+// // }
+
+// dice.GetBalance(tryMe, data.Balance);
+// Account balance
+// function getBook(callback) {
+//   dice.GetBalance(dice.ContractAddress, function (data) {
+//     $('.accountBalance').text(data.Balance)
+//   })
+// }
+// getBook(function (data) {
+//   console.dir(data.Balance)
+//   $('.accountBalance').text(data.Balance)
+// });
+$(document).ready(function ($) {
   // tab
   $('#tab').tabulous({
     effect: 'slideLeft'
@@ -12,96 +51,96 @@ $(document).ready(function($) {
     effect: 'flip'
   })
   // progress
-  var scroll = document.getElementById('scroll');
-  var bar = document.getElementById('bar');
-  var mask = document.getElementById('mask');
-  var ptxt = document.getElementsByTagName('p')[0];
-  var barleft = 0;
-  ptxt.innerHTML = "0";
+  var scroll = document.getElementById('scroll')
+  var bar = document.getElementById('bar')
+  var mask = document.getElementById('mask')
+  var ptxt = document.getElementsByTagName('p')[0]
+  var barleft = 0
+  ptxt.innerHTML = '0'
   // setWin
-  $('.setWin').text('0');
+  $('.setWin').text('0')
   // setChance
-  $('.setChance').text('0');
-  bar.onmousedown = function(event) {
-    var event = event || window.event;
-    var leftVal = event.clientX - this.offsetLeft;
-    var that = this;
-    document.onmousemove = function(event) {
-      var event = event || window.event;
+  $('.setChance').text('0')
+  bar.onmousedown = function (event) {
+    var event = event || window.event
+    var leftVal = event.clientX - this.offsetLeft
+    var that = this
+    document.onmousemove = function (event) {
+      var event = event || window.event
       var currentVal = $('#scroll p').text()
-      barleft = event.clientX - leftVal;
+      barleft = event.clientX - leftVal
       if (barleft < 0) {
-        barleft = 0;
+        barleft = 0
       } else if (barleft > scroll.offsetWidth - bar.offsetWidth) {
-        barleft = scroll.offsetWidth - bar.offsetWidth;
+        barleft = scroll.offsetWidth - bar.offsetWidth
       }
       if (ptxt.innerHTML == '100') {
-        mask.style.width = barleft + 20 + 'px';
-        ptxt.style.left = barleft - 10 + "px";
-        that.style.left = barleft + 10 + "px";
+        mask.style.width = barleft + 20 + 'px'
+        ptxt.style.left = barleft - 10 + 'px'
+        that.style.left = barleft + 10 + 'px'
       } else if (ptxt.innerHTML == '0') {
-        mask.style.width = barleft + 10 + 'px';
-        ptxt.style.left = "-26px";
-        that.style.left = "-7px";
+        mask.style.width = barleft + 10 + 'px'
+        ptxt.style.left = '-26px'
+        that.style.left = '-7px'
         $('.getOdds').text('0')
         // setWin
-        $('.setWin').text('0');
+        $('.setWin').text('0')
         // setChance
-        $('.setChance').text('0');
+        $('.setChance').text('0')
       } else {
-        mask.style.width = barleft + 10 + 'px';
-        ptxt.style.left = barleft - 18 + 'px';
-        that.style.left = barleft + 'px';
+        mask.style.width = barleft + 10 + 'px'
+        ptxt.style.left = barleft - 18 + 'px'
+        that.style.left = barleft + 'px'
         // odds calculation
         $('.getOdds').text((100).division(currentVal).toFixed(3))
       }
-      ptxt.innerHTML = parseInt(barleft / (scroll.offsetWidth - bar.offsetWidth) * 100);
+      ptxt.innerHTML = parseInt(barleft / (scroll.offsetWidth - bar.offsetWidth) * 100)
       // setWin
-      $('.setWin').text(ptxt.innerHTML);
+      $('.setWin').text(ptxt.innerHTML)
       // setChance
-      $('.setChance').text(ptxt.innerHTML - 1);
-      window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+      $('.setChance').text(ptxt.innerHTML - 1)
+      window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty()
     }
 
   }
-  document.onmouseup = function() {
-    document.onmousemove = null;
+  document.onmouseup = function () {
+    document.onmousemove = null
   }
   // repair javascript Floating-point number bug
   function accMul(arg1, arg2) {
     var m = 0,
-      s1 = arg1.toString(),
-      s2 = arg2.toString();
+      s1 = arg1.toString()
+    s2 = arg2.toString()
     try {
-      m += s1.split(".")[1].length
+      m += s1.split('.')[1].length
     } catch (e) {}
     try {
-      m += s2.split(".")[1].length
+      m += s2.split('.')[1].length
     } catch (e) {}
-    return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
+    return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m)
   }
-  Number.prototype.mul = function(arg) {
+  Number.prototype.mul = function (arg) {
     return accMul(arg, this);
   }
 
   function accDiv(arg1, arg2) {
     var t1 = 0,
       t2 = 0,
-      r1, r2;
+      r1, r2
     try {
-      t1 = arg1.toString().split(".")[1].length;
+      t1 = arg1.toString().split('.')[1].length
     } catch (e) {}
     try {
-      t2 = arg2.toString().split(".")[1].length;
+      t2 = arg2.toString().split('.')[1].length
     } catch (e) {}
     with(Math) {
-      r1 = Number(arg1.toString().replace(".", ""));
-      r2 = Number(arg2.toString().replace(".", ""));
+      r1 = Number(arg1.toString().replace('.', ''))
+      r2 = Number(arg2.toString().replace('.', ''))
       return (r1 / r2) * pow(10, t2 - t1);
     }
   }
-  Number.prototype.division = function(arg) {
-    return accDiv(this, arg);
+  Number.prototype.division = function (arg) {
+    return accDiv(this, arg)
   }
 
   // bets click
@@ -114,17 +153,18 @@ $(document).ready(function($) {
     if (isLogin == true) {
       var balance = 20000000
       var mostBets = balance / 2
+      // $('.accountBalance').text()
       if (indemnity > mostBets) {
         var indemnity = betsSeele * getOdds
         $('.winSeele').text(indemnity)
-        $('.bets ul li').click(function() {
+        $('.bets ul li').click(function () {
           $(this).siblings('li').removeClass('current')
           $(this).addClass('current')
         })
         $('.getVal').val(balance / 4)
         $('.winSeele').text(balance / 2)
       } else {
-        $('.halve').click(function() {
+        $('.halve').click(function () {
           var getOdds = $('.getOdds').text()
           var indemnity = betsSeele * getOdds
           $('.winSeele').text(indemnity)
@@ -134,7 +174,7 @@ $(document).ready(function($) {
           $('.getVal').val($('.getVal').val() / 2)
           $('.winSeele').text((betsSeeleHalve).mul(getOdds))
         })
-        $('.multiple').click(function() {
+        $('.multiple').click(function () {
           var getOdds = $('.getOdds').text()
           var indemnity = betsSeele * getOdds
           $('.winSeele').text(indemnity)
@@ -144,7 +184,7 @@ $(document).ready(function($) {
           $('.getVal').val($('.getVal').val() * 2)
           $('.winSeele').text((betsSeeleMul).mul(getOdds))
         })
-        $('.all').click(function() {
+        $('.all').click(function () {
           var getOdds = $('.getOdds').text()
           var indemnity = betsSeele * getOdds
           $('.winSeele').text(indemnity)
@@ -156,7 +196,7 @@ $(document).ready(function($) {
         })
       }
     } else {
-      $('.halve').click(function() {
+      $('.halve').click(function () {
         var getOdds = $('.getOdds').text()
         var indemnity = betsSeele * getOdds
         $('.winSeele').text(indemnity)
@@ -165,7 +205,7 @@ $(document).ready(function($) {
         $('.getVal').val($('.getVal').val() / 2)
         $('.winSeele').text(indemnity)
       })
-      $('.multiple').click(function() {
+      $('.multiple').click(function () {
         var getOdds = $('.getOdds').text()
         var indemnity = betsSeele * getOdds
         $('.winSeele').text(indemnity)
@@ -174,7 +214,7 @@ $(document).ready(function($) {
         $('.getVal').val($('.getVal').val() * 2)
         $('.winSeele').text(indemnity)
       })
-      $('.all').click(function() {
+      $('.all').click(function () {
         $('.login').show()
       })
     }
@@ -188,27 +228,142 @@ $(document).ready(function($) {
   $('.transaction').hide()
   $('.login').hide()
   $('.playPopup').hide()
-  // login close
-  $('.login .close,.loginTab_container button').click(function() {
-    $('.login').hide()
-    $('.loginImg').show()
-    $('.loginHeadButton').hide()
-    $('.rollButton').show()
-    $('.loginButton').hide()
-  })
   // show login
-  $('.loginButton button,.quicklink button').click(function() {
+  $('.login').click(function () {
     $('.login').show()
   })
+  // login close
+  $('.close').click(function () {
+    $('.login').hide()
+  })
+
+  // 128-bit key
+  var key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+  // aes encryption
+  function aesEncryption(text) {
+    // Convert text to bytes
+    var textBytes = aesjs.utils.utf8.toBytes(text)
+    // The counter is optional, and if omitted will begin at 1
+    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5))
+    var encryptedBytes = aesCtr.encrypt(textBytes);
+    // To print or store the binary data, you may convert it to hex
+    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes)
+    return encryptedHex
+  }
+  // aes Decrypt
+  function aesDecrypt(encryptedHex) {
+    // When ready to decrypt the hex string, convert it back to bytes
+    var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex)
+    // The counter mode of operation maintains internal state, so to
+    // decrypt a new instance must be instantiated.
+    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5))
+    var decryptedBytes = aesCtr.decrypt(encryptedBytes)
+    // Convert our bytes back into text
+    var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes)
+    return decryptedText
+  }
+  // Start with 0x,English and integer only.     
+  jQuery.validator.addMethod('isSpecialChar', function (value, element) {
+    return this.optional(element) || /^0x[A-Za-z0-9_-]+$/.test(value)
+  }, 'Start with 0x,English and integer only.')
+  // return validform result
+  function validform() {
+    return $('#loginValidate').validate({
+      debug: true,
+      onkeyup: false,
+      rules: {
+        username: {
+          required: true,
+          isSpecialChar: true,
+          minlength: '32'
+        },
+        password: {
+          required: true
+        }
+      },
+      messages: {
+        username: {
+          required: 'Keyfile can not be empty.',
+          rangelength: 'English and numeric input only.',
+          isSpecialChar: 'Start with 0x,English and integer only.',
+          minlength: 'Please input not less than 32 characters.'
+        },
+        password: {
+          required: 'Password can not be empty.'
+        }
+      }
+    })
+  }
+  $(validform())
+
+  // get SessionStorage
+  var userJsonStrUser = JSON.parse(sessionStorage.getItem('user'))
+
+  // Numerical conversion
+  function balanceValueInteger(value) {
+    var stringVal = (value / 100000000).toString()
+    if (!/^\d+$/.test(stringVal)) {
+      var valueSplit = stringVal.split('.')
+      var integer = valueSplit[0]
+      return formatNumber(integer)
+    } else if (/^\d+$/.test(stringVal)) {
+      return formatNumber(value / 100000000)
+    } else {
+      return formatNumber(value)
+    }
+  }
+  function balanceValueDecimal(value) {
+    var stringVal = (value / 100000000).toString()
+    if (!/^\d+$/.test(stringVal)) {
+      var valueSplit = stringVal.split('.')
+      var decimal = valueSplit[1]
+      return '.' + decimal
+    } else if (/^\d+$/.test(stringVal)) {
+      return ''
+    } else {
+      return ''
+    }
+  }
+
+  // click login
+  $('#loginTab-1 button,.loginButton button,.quicklink button').click(function () {
+    if (validform().form()) {
+      var getUsername = $('#username').val()
+      var getPrivate = aesEncryption($('#private').val())
+      var getPassword = $('#password').val()
+      var keyArray = {
+        username: getUsername,
+        private: getPrivate,
+        password: getPassword
+      }
+      sessionStorage.setItem('user', JSON.stringify(keyArray))
+      var userJsonStrUser = JSON.parse(sessionStorage.getItem('user'))
+      var getStorageUsername = userJsonStrUser.username
+      $('.getUserName').text(getStorageUsername)
+      dice.GetBalance(getStorageUsername, function (data) {
+        $('.login').hide()
+        $('.loginImg').show()
+        $('.loginHeadButton').hide()
+        $('.rollButton').show()
+        $('.loginButton').hide()
+        if (data instanceof Error) {
+          $('.accountBalance').text('0')
+          return
+        } else {
+          $('.accountBalance').text(data.Balance)
+        }
+      })
+    }
+  })
   // longinImg mouse
-  $('.loginImg').mouseover(function() {
+  $('.loginImg').mouseover(function () {
     $('.personalInformation').show()
   })
-  $(document).bind("click", function() {
+  $(document).bind('click', function () {
     $('.personalInformation').hide()
   })
   // logout
-  $('.logout').click(function() {
+  $('.logout').click(function () {
     $('.personalInformation').hide()
     $('.loginImg').hide()
     $('.loginHeadButton').show()
@@ -216,50 +371,122 @@ $(document).ready(function($) {
     $('.rollButton').hide()
   })
   // transaction popup
-  $('.rollButton button').click(function() {
+  $('.rollButton button').click(function () {
+    // pulic
+    var bet = $('.getVal').val()
+    var roll = $('.setWin').text()
+    var payout = $('.winSeele').text()
+    var gasPricePost = 1
+    var gasLimitPost = 3000000
+
+    // get username
+    var getStorageUsername = userJsonStrUser.username
+
+    $('.from').text(getStorageUsername)
+    $('.betAmount').text(bet)
+    $('.payOut').text(payout)
+    $('.gasPrice').text(gasPricePost)
+    $('.gasLimit').text(gasLimitPost)
+    $('.rollUnder').text(roll)
+
+    // get nonce
+    dice.GetAccountNonce(getStorageUsername, function (data) {
+      console.log('callback')
+      console.log(data)
+      if (data instanceof Error) {
+        console.log('callback Error')
+        return
+      }
+      console.log('callback Success')
+      $('.transactionMain table tr:nth-child(4) td:nth-child(2)').text(data)
+    })
+
     $('.transaction').show()
   })
   // close transction
-  $('.transaction ul li:nth-child(1) button,.transaction .close').click(function() {
+  $('.transaction ul li:nth-child(1) button,.transaction .close').click(function () {
     $('.transaction').hide()
   })
   // post transction data
-  $('.transaction ul li:nth-child(2) button').click(function() {
+  $('.transaction ul li:nth-child(2) button').click(function () {
+    // get private
+    var getStoragePrivate = aesDecrypt(userJsonStrUser.private)
+
     // post data
-    // var from = $('.transactionMain table tr:nth-child(1) td:nth-child(2)').text()
-    // var to = $('.transactionMain table tr:nth-child(2) td:nth-child(2)').text()
-    // var amount = $('.transactionMain table tr:nth-child(3) td:nth-child(2)').text()
-    // var accountNonce = $('.transactionMain table tr:nth-child(4) td:nth-child(2)').text()
-    // var quantity = $('.transactionMain table tr:nth-child(5) td:nth-child(2)').text()
-    // var gasPrice = $('.transactionMain table tr:nth-child(6) td:nth-child(2)').text()
-    // var gasLimit = $('.transactionMain table tr:nth-child(7) td:nth-child(2)').text()
-    // var payload = $('.transactionMain table tr:nth-child(8) td:nth-child(2)').text()
+    var from = $('.from').text()
+    var betAmount = $('.betAmount').text()
+    var rollPayout = $('.payOut').text()
+    var gasPrice = $('.gasPrice').text()
+    var gasLimit = $('.gasLimit').text()
+    var rollUnder = $('.rollUnder').text()
+
+    // sendTx
+    var keypair = {
+      'PublicKey': from,
+      'PrivateKey': getStoragePrivate
+    }
+    var args = {
+      'RollUnder': rollUnder,
+      'Payout': rollPayout,
+      'Bet': betAmount,
+      'GasPrice': gasPrice,
+      'GasLimit': gasLimit
+    }
+    dice.Sendtx(keypair, args, function (data) {
+      if (data instanceof Error) {
+        console.log('callback Error')
+        return
+      } else {
+        dice.GetReceipt(data, function (data) {
+          console.log("callback")
+          console.log(data)
+          if (data instanceof Error) {
+            console.log("callback Error")
+            return
+          }
+          $('.listTime').text(data.Time)
+          $('.listBet').text(data.Bettor)
+          $('.listRollUnder').text(data.RollUnder)
+          $('.listBet').text(balanceValueInteger(data.Bet) + '.' + balanceValueDecimal(data.Bet))
+          $('.listRoll').text(data.Roll)
+          $('.listPayout').text(balanceValueInteger(data.Payout) + '.' + balanceValueDecimal(data.Payout))
+          if(data.Event == 'winAction') {
+            $('.result').text('Congratulations on winning.')
+          }
+          else if(data.Event == 'lossAction'){
+            $('.result').text("I'm sorry you failed.")
+          }
+          console.log('callback Success')
+        })
+      }
+      console.log('callback Success')
+    })
     $('.transaction').hide()
   })
   // close playPopup
-  $('.playPopup .close').click(function() {
+  $('.playPopup .close').click(function () {
     $('.playPopup').hide()
   })
   // show how to play
-  $('.play').click(function() {
+  $('.play').click(function () {
     $('.playPopup').show()
   })
-  // file name
+  // get file name
   function getFileName(path) {
-    var pos1 = path.lastIndexOf('/');
-    var pos2 = path.lastIndexOf('\\');
-    var pos = Math.max(pos1, pos2);
+    var pos1 = path.lastIndexOf('/')
+    var pos2 = path.lastIndexOf('\\')
+    var pos = Math.max(pos1, pos2)
     if (pos < 0) {
-      return path;
+      return path
     } else {
-      return path.substring(pos + 1);
+      return path.substring(pos + 1)
     }
   }
-    $('.getImageName').change(function() {
-      var str = $(this).val();
-      var fileName = getFileName(str);
-      var fileExt = str.substring(str.lastIndexOf('.') + 1);
-      $('.setImageName').val(fileName)
-      // console.log(fileName + "\r\n" + fileExt);
-  });
-});
+  $('.getImageName').change(function () {
+    var str = $(this).val()
+    var fileName = getFileName(str)
+    var fileExt = str.substring(str.lastIndexOf('.') + 1)
+    $('.setImageName').val(fileName)
+    // console.log(fileName + '\r\n' + fileExt);
+  })
+})
