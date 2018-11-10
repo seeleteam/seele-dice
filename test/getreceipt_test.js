3204c7f37e1d51c3e3974915afbcee4f96b2117f
wangff15386 <314130948@qq.com>
2018-11-10 19:19:46 +0800

    upgrade the dice server

--- test/getreceipt_test.js
+++ test/getreceipt_test.js
@@ -1,6 +1,6 @@
 const dice = require("../dice/dice")
 
-dice.GetReceipt("0xc6d83fb560857b346bfa6cc2a789c3cfc19b8dc9f264dc9bdd7c31a28d435df6", function(data){
+dice.GetReceipt("0x239415713dc0b3fbb605e9784593a2b6b130c0498e470e5a82fd3f75823c850e", function(data){
     console.log("callback")
     console.log(data)
     if (data instanceof Error){
